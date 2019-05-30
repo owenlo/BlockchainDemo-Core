bed848cd1571c95b18ceb7b9b7426f070db41d28
owenlo <owenlo@gmail.com>
2019-05-30 12:38:32 +0100

    Further UI cleanup

--- BlockchainDemo.WebAPI/wwwroot/scripts/site.js
+++ BlockchainDemo.WebAPI/wwwroot/scripts/site.js
@@ -1,55 +1,53 @@
-﻿$(document).ready(function () {
+﻿$(document).ready(function () {   
+    $.ajax({
+        type: 'GET',
+        url: 'api/blocks',
+        dataType: 'json',
+        success: function (data) {
+            $.each(data, function (index, value) {
+                addBlock(value);
 
-            $("#addblock").click(function () {
-                $.ajax({
-                    type: 'post',
-                    url: 'api/blocks',
-                    dataType: 'json',
-                    contentType: 'application/json',
-                    data: JSON.stringify( { "data": $('#datainput').val(), "prevhash": $('#prevhashinput').val(), "difficulty": $('#difficultyinput').val() } ),
-                    processData: false,
-                    success: function( data, textStatus, jQxhr ){
-                        addBlock(data);
-
-                        $('#datainput').val("");
-                        $('#prevhashinput').val(data.hash);
-                    },
-                    error: function( jqXhr, textStatus, errorThrown ){
-                        console.log( errorThrown );
-                        console.log($('#datainput').val());
-                    }
-                });
-            });
-
-            $.ajax({
-                type: 'GET',
-                url: 'api/blocks',
-                dataType: 'json',
-                success: function (data) {
-                    $.each(data, function (index, value) {
-                        addBlock(value);
-
-                        $('#prevhashinput').val(value.hash);
-                    });
+                if (index === data.length - 1) {
+                    $('#prevhashinput').val(value.hash);
                 }
             });
+        }
+    });
+
+    $("#addblock").click(function () {
+        event.preventDefault();
+
+        $.ajax({
+            type: 'post',
+            url: 'api/blocks',
+            dataType: 'json',
+            contentType: 'application/json',
+            data: JSON.stringify({ "data": $('#datainput').val(), "prevhash": $('#prevhashinput').val(), "difficulty": $('#difficultyinput').val() }),
+            processData: false,
+            success: function (data, textStatus, jQxhr) {
+                addBlock(data);
+
+                $('#datainput').val("");
+                $('#prevhashinput').val(data.hash);
+            },
+            error: function (jqXhr, textStatus, errorThrown) {
+                console.log(errorThrown);
+            }
         });
+    });
+});
 
-        function addBlock(value) {
-            var tr = $('<tr/>');
+function addBlock(value) {
+    var tr = $('<tr/>');
 
-            tr.append('<td>' + value.id + '</td>');
-            tr.append('<td>' + value.data + '</td>');
-            tr.append('<td>' + trim(value.hash, value.difficulty) + '...</td>');
-            tr.append('<td>' + trim(value.prevhash, value.difficulty) + '...</td>');
-            tr.append('<td>' + value.timestamp + '</td>');
-            tr.append('<td>' + value.difficulty + '</td>');
-            tr.append('<td>' + value.nonce + '</td>');
-            tr.append('<td>' + Math.round(value.performance * 100) / 100 + '</td>');
+    tr.append('<td>' + value.id + '</td>');
+    tr.append('<td>' + value.data + '</td>');
+    tr.append('<td>' + value.hash + '</td>');
+    tr.append('<td>' + value.prevhash + '</td>');
+    tr.append('<td>' + value.timestamp + '</td>');
+    tr.append('<td>' + value.difficulty + '</td>');
+    tr.append('<td>' + value.nonce + '</td>');
+    tr.append('<td>' + Math.round(value.performance * 100) / 100 + '</td>');
 
-            $('#blocks').append(tr);
-        }
-
-        function trim(string, length) {
-            return string.substring(0, length + 5);
-        }
+    $('#blocks').append(tr);
+}
